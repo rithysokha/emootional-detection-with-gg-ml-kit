3e4379a398cf7cbe7649e40d1246f0aad123ce47
J3llyfi5h <lovely18012004@gmail.com>
2025-01-24 18:59:05 +0700

    fix ui

--- app/src/main/java/com/emotion/detector/PushNotificationService.kt
+++ app/src/main/java/com/emotion/detector/PushNotificationService.kt
@@ -19,11 +19,6 @@ class PushNotificationService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d("FCM", "From: ${remoteMessage.from}")
 
-        remoteMessage.notification?.let {
-            Log.d("FCM", "Message Notification Body: ${it.body}")
-            sendNotification(it.body)
-        }
-
         val title = remoteMessage.notification?.title ?: "No Title"
         val description = remoteMessage.notification?.body ?: "No Description"
         val imageUrl = remoteMessage.notification?.imageUrl?.toString()
@@ -40,27 +35,4 @@ class PushNotificationService : FirebaseMessagingService() {
         }
     }
 
-    private fun sendNotification(messageBody: String?) {
-        val channelId = "default_channel_id"
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                "Default Channel",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            notificationManager.createNotificationChannel(channel)
-        }
-
-        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.ic_notifications_black_24dp)
-            .setContentTitle("App Update")
-            .setContentText(messageBody)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setAutoCancel(true)
-
-        notificationManager.notify(0, notificationBuilder.build())
-    }
 }
